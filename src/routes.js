68ff8c3e2af991dd4f85a4b546061058f9724538
IngridCosta23 <ingridmoreiracosta23@gmail.com>
2022-11-22 19:18:21 -0300

    refactor: Refatorando o codigo

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio/>}/>
-        <Route path='/cardapio' element={<Cardapio/>}/>
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu/>
+        <Routes>
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio/>}></Route>
+            <Route path='cardapio' element={<Cardapio/>}/>
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
