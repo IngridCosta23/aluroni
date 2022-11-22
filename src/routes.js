631b3574939a14ebb8b65419e4bbe7f8679a7eb3
IngridCosta23 <ingridmoreiracosta23@gmail.com>
2022-11-22 20:07:25 -0300

    feat: Tela Sobre

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
 
 
 export default function AppRouter() {
@@ -14,6 +15,7 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao/>}>
             <Route index element={<Inicio/>}></Route>
             <Route path='cardapio' element={<Cardapio/>}/>
+            <Route path='sobre' element={<Sobre/>}/>
           </Route>
         </Routes>
       </Router>
